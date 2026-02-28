75eba741edf3f5a305844339b250e48398e1d21a
Jeongmin Mo <jmmo0722@gmail.com>
2026-03-01 00:18:04 +0900

    [level 2] Title: 타겟 넘버, Time: 4.73 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -2,28 +2,28 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
+    
+    static int count;
     public int solution(int[] numbers, int target) {
-        int answer = 0;
         
-        Deque<int[]> que = new ArrayDeque<>();
-        que.add(new int[]{numbers[0], 0});
-        que.add(new int[]{-numbers[0], 0});
+        count = 0;
         
-        while(!que.isEmpty()){
-            int[] poll = que.poll();    
-            int curSum = poll[0];
-            int curIndex = poll[1];
-            
-            if(curSum == target && curIndex == numbers.length-1){
-                answer++;
-            }
-            
-            if(curIndex < numbers.length-1){
-                que.offer(new int[]{curSum + (numbers[curIndex+1]), curIndex+1});
-                que.offer(new int[]{curSum + (-numbers[curIndex+1]), curIndex+1});
-            }
+        dfs(1, numbers[0], numbers, target);
+        dfs(1, -numbers[0], numbers, target);
+        
+        return count;
+    }
+    
+    public static void dfs(int index, int sum, int[] numbers, int target){
+        
+        if(index > numbers.length-1 && sum == target){
+            count++;
         }
         
-        return answer;
-    }
+        if(index > numbers.length-1) return;
+
+        dfs(index+1, sum+numbers[index], numbers, target);
+        dfs(index+1, sum-numbers[index], numbers, target);
+        
+    } 
 }
